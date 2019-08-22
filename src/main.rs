d7dc01557b8137eef41bc374c4442c36776a275e
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-08-22 10:33:05 -0700

    Handle being passed zero parameters

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,26 @@ mod config;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "Homebound", about = "A cross-platform symlink manager")]
-struct Opt {
-    /// The config file to use.
-    #[structopt(short = "c", long = "config")]
-    config: String,
+enum Homebound {
+    #[structopt(name = "install")]
+    Install {
+        /// The config file to use.
+        #[structopt(short = "c", long = "config", default_value = "config.yaml")]
+        config: String,
 
-    /// Whether to do a dry run or not. A dry run will not modify system files or create symlinks.
-    #[structopt(short = "d", long = "dry")]
-    dry: bool,
+        /// Whether to do a dry run or not. A dry run will not modify system files or create symlinks.
+        #[structopt(short = "d", long = "dry")]
+        dry: bool,
+    },
 }
 
 fn main() {
-    let opt = Opt::from_args();
-    let config = Config::new(&opt.config).unwrap();
-    config.apply(opt.dry).unwrap();
+    match Homebound::from_args() {
+        Homebound::Install { config, dry } => {
+            let config_data = Config::new(&config).expect("Could not create configuration");
+            config_data
+                .apply(dry)
+                .expect("Error applying configuration");
+        }
+    }
 }
